a8c71e4e46e92e7e4d6524c95e6f1915c7489936
Ilya Aleksandrov <i.m.alexandrov@yandex.ru>
2022-03-18 12:58:56 +0500

    Authorization On Twitch. Suscces and Failed(UknkownUser)

---
+++ src/test/java/tv/twitch/AuthorizationOnTwitch.java
@@ -0,0 +1,57 @@
+package tv.twitch;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class AuthorizationOnTwitch {
+
+    @Test
+    public void successAuthorizationOnTwitch() {
+        System.setProperty("webdriver.opera.driver","/WebDrivers/operadriver_win64/operadriver.exe");
+        OperaDriver driver = new OperaDriver();
+        driver.get("https://www.twitch.tv/");
+        WebElement AnonUserLoginButtonFirstClick = driver.findElement(By.cssSelector("[data-test-selector='anon-user-menu__login-button']"));
+        AnonUserLoginButtonFirstClick.click();
+        WebElement userName = (WebElement) new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@autocomplete='username']")));
+        userName.sendKeys("TestUserrr2");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@autocomplete='current-password']"));
+        userPassword.sendKeys("321654987Qwer!-!");
+        WebElement AnonUserLoginButtonSecondClick = driver.findElement(By.xpath("//button[@data-a-target='passport-login-button']"));
+        AnonUserLoginButtonSecondClick.click();
+        WebElement WelcomeText = (WebElement) new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[@id='modal-root-header']")));
+        String expectedText = driver.findElement(By.xpath("//h3[@id='modal-root-header']")).getText();
+        String actualText = "Добро пожаловать на вечеринку, testuserrr2!";
+        assertEquals(expectedText, actualText.strip());
+
+    }
+
+    @Test
+    public void failedAuthorizationOnTwitchUnknownUser() {
+        System.setProperty("webdriver.opera.driver","/WebDrivers/operadriver_win64/operadriver.exe");
+        OperaDriver driver = new OperaDriver();
+        driver.get("https://www.twitch.tv/");
+        WebElement AnonUserLoginButtonFirstClick = driver.findElement(By.cssSelector("[data-test-selector='anon-user-menu__login-button']"));
+        AnonUserLoginButtonFirstClick.click();
+        WebElement userName = (WebElement) new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@autocomplete='username']")));
+        userName.sendKeys("T");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@autocomplete='current-password']"));
+        userPassword.sendKeys("321654987Qwer!-!");
+        WebElement AnonUserLoginButtonSecondClick = driver.findElement(By.xpath("//button[@data-a-target='passport-login-button']"));
+        AnonUserLoginButtonSecondClick.click();
+        WebElement errorText = (WebElement) new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//Strong[@class='CoreText-sc-cpl358-0 kUHrUR']")));
+        String actualText = "Такого имени пользователя не существует.";
+        String expectedText = driver.findElement(By.xpath("//Strong[@class='CoreText-sc-cpl358-0 kUHrUR']")).getText();
+        assertEquals(expectedText, actualText.strip());
+    }
+
+}
